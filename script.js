80b792014202c39acf5d2fc4c541f055cbebfdb4
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-08 18:03:00 -0300

    melhorando

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const btnConfirmar = document.getElementById('confirmar-zap');
 const btnFechar = document.getElementById('fechar-modal');
 
 // Configurações
-const numeroTelefone = "5511999999999"; // Coloque o número aqui (só números) nescessario ter o 55 ou pais e o 11 ou otra cidade
+const numeroTelefone = "5511999999999"; // 55 + DDD + número
 const mensagem = encodeURIComponent("Olá, tudo bem?");
 
 // Abrir modal
@@ -17,13 +17,13 @@ btnFechar.onclick = () => {
     modal.style.display = "none";
 };
 
-// Redirecionar para o Zap
+// Ir para o WhatsApp
 btnConfirmar.onclick = () => {
     window.open(`https://wa.me/${numeroTelefone}?text=${mensagem}`, '_blank');
     modal.style.display = "none";
 };
 
-// Fechar se clicar fora da modal
+// Fechar clicando fora
 window.onclick = (event) => {
-    if (event.target == modal) modal.style.display = "none";
+    if (event.target === modal) modal.style.display = "none";
 };
